aad3e79a6c9d4c3bad53ee2dc0b993f270ca177e
Joanna Wheeler <joannachristinewheeler@gmail.com>
2018-07-12 12:14:00 -0700

    added s to whoops for test

--- publishers-master/resources/views/_landing_styles.blade.php
+++ publishers-master/resources/views/_landing_styles.blade.php
@@ -1,4 +1,4 @@
-<!-- test first commit whoopssss ->
+<!-- test first commit whoopssssss ->
 
     <!-- Bootstrap core CSS -->
     <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
